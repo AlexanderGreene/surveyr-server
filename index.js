494aad33a8eeab39a102624fbcb540033412ced0
Alex Greene <Alex.Greene@wendys.com>
2025-07-22 08:36:25 -0400

    Add first route (demo) specify node versions in package.json

--- index.js
+++ index.js
@@ -2,12 +2,12 @@ const express = require('express');
 
 const app = express();
 
-const port = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000;
 
 app.get('/', (req, res) => {
 	res.send({ hi: 'there' });
 });
 
-app.listen(port, () => {
-	console.log(`Server running on port ${port}`);
+app.listen(PORT, () => {
+	console.log(`Server running on port ${PORT}`);
 });
