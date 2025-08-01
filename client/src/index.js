e4324040bab6abef47d13892c4145f6f3ca9a090
Alex Greene <Alex.Greene@wendys.com>
2025-08-01 12:47:21 -0400

    refactor to use async/await in server remove and replace client files created by CRA set up basic redux boilerplate code create authReducer.js

---
+++ client/src/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+
+import App from './components/App';
+import reducers from './reducers';
+
+const el = document.getElementById('root');
+
+const root = ReactDOM.createRoot(el);
+
+const store = createStore(reducers, {}, applyMiddleware());
+
+root.render(
+	<Provider store={store}>
+		<App />
+	</Provider>
+);
